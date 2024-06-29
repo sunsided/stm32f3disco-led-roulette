e9d6383f5c1ba25d1d4c5e3d834546c6ffcbfb03
Markus Mayer <widemeadows@gmail.com>
2024-06-29 18:34:42 +0200

    Use low-level packages

--- bins/led_roulette/src/main.rs
+++ bins/led_roulette/src/main.rs
@@ -1,59 +1,43 @@
+//! Uses the StatefulOutputPin embedded_hal trait to toggle the pin
+//! On the stm32 discovery board this is the "south" led
+//! Target board: STM32F3DISCOVERY
+
+#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
-#![deny(unsafe_code)]
 
-use stm32f3_discovery::cortex_m;
-use stm32f3_discovery::cortex_m_rt::entry;
-use stm32f3_discovery::leds::Leds;
-use stm32f3_discovery::stm32f3xx_hal::gpio::{gpioe, Output, PushPull};
-use stm32f3_discovery::stm32f3xx_hal::{delay::Delay, pac, prelude::*};
-use stm32f3_discovery::switch_hal::{ActiveHigh, OutputSwitch, Switch};
+use panic_semihosting as _;
+
+use stm32f3xx_hal as hal;
+
+use cortex_m_rt::entry;
+use hal::pac;
+use hal::prelude::*;
 
 #[entry]
 fn main() -> ! {
-    let (mut delay, mut leds): (Delay, LedArray) = init();
+    let dp = pac::Peripherals::take().unwrap();
 
-    let ms = 30_u8;
+    let mut rcc = dp.RCC.constrain();
+    let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
+
+    let mut led = gpioe
+        .pe13
+        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+
+    led.set_low().unwrap();
+
     loop {
-        for curr in 0..8 {
-            let next = (curr + 1) % 8;
-
-            leds[next].on().ok();
-            delay.delay_ms(ms);
-
-            leds[curr].off().ok();
-            delay.delay_ms(ms);
+        led.toggle().unwrap();
+        cortex_m::asm::delay(8_000_000);
+        // Toggle by hand.
+        // Uses `StatefulOutputPin` instead of `ToggleableOutputPin`.
+        // Logically it is the same.
+        if led.is_set_low().unwrap() {
+            led.set_high().unwrap();
+        } else {
+            led.set_low().unwrap();
         }
+        cortex_m::asm::delay(8_000_000);
     }
-}
-
-/// The array of 8 LEDs available on the STM32F3 Discovery board.
-pub type LedArray = [Switch<gpioe::PEx<Output<PushPull>>, ActiveHigh>; 8];
-
-/// Initializes the MCU and prepares the LED array.
-pub fn init() -> (Delay, LedArray) {
-    let device_periphs = pac::Peripherals::take().unwrap();
-    let mut reset_and_clock_control = device_periphs.RCC.constrain();
-
-    let core_periphs = cortex_m::Peripherals::take().unwrap();
-    let mut flash = device_periphs.FLASH.constrain();
-    let clocks = reset_and_clock_control.cfgr.freeze(&mut flash.acr);
-    let delay = Delay::new(core_periphs.SYST, clocks);
-
-    // initialize user leds
-    let mut gpioe = device_periphs.GPIOE.split(&mut reset_and_clock_control.ahb);
-    let leds = Leds::new(
-        gpioe.pe8,
-        gpioe.pe9,
-        gpioe.pe10,
-        gpioe.pe11,
-        gpioe.pe12,
-        gpioe.pe13,
-        gpioe.pe14,
-        gpioe.pe15,
-        &mut gpioe.moder,
-        &mut gpioe.otyper,
-    );
-
-    (delay, leds.into_array())
 }
